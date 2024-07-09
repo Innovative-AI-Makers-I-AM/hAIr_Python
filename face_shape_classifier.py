257f036be23057c85a370516a4e7908b217e7032
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2024-07-09 16:03:08 +0900

    feat: [안성희] "얼굴형 분석 모델과 fastapi연결"

--- face_shape_classifier.py
+++ face_shape_classifier.py
@@ -12,8 +12,17 @@ def load_image(image_path):
     return Image.open(image_path)
 
 # 예측 함수
-def predict(image_path):
-    image = load_image(image_path)
+# def predict(image_path):
+#     image = load_image(image_path)
+#     inputs = processor(images=image, return_tensors="pt")
+#     with torch.no_grad():
+#         outputs = model(**inputs)
+#     logits = outputs.logits
+#     predicted_class_idx = logits.argmax(-1).item()
+#     return model.config.id2label[predicted_class_idx]
+
+# 이미지 경로 없는 예측 함수 
+def predict(image: Image.Image) -> str:
     inputs = processor(images=image, return_tensors="pt")
     with torch.no_grad():
         outputs = model(**inputs)
@@ -22,6 +31,7 @@ def predict(image_path):
     return model.config.id2label[predicted_class_idx]
 
 # 예측 수행
-image_path = "images\KakaoTalk_20240709_134904035.jpg"  # 예측할 이미지 경로
-predicted_label = predict(image_path)
-print(f"Predicted label: {predicted_label}")
+
+# image_path = "images\KakaoTalk_20240709_134904035.jpg"  # 예측할 이미지 경로
+# predicted_label = predict(image_path)
+# print(f"Predicted label: {predicted_label}")
