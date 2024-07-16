c7f352df8e970d45021e2f6211b5ac4391241610
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2024-07-16 17:03:40 +0900

    update: [안성희] "이미지 데이터와 메타데이터를 같이 저장"

--- PostgreSQL/PostgreSQL_main.py
+++ PostgreSQL/PostgreSQL_main.py
@@ -1,11 +1,22 @@
 from database import init_db
-from images_utils import save_image_to_db, delete_all_images, delete_images_by_id_range, show_image, list_images
+from images_utils import save_image_to_db, delete_all_images, delete_images_by_id_range, show_image, list_images, save_image_metadata_to_db
 
 # 데이터베이스 초기화
 init_db()
 
 # 예제: 이미지 저장
 image_path = 'F:/Repository/hAIr_Python/images/Autumn Gaze.jpg'  # 실제 경로로 변경하세요
+
+metadata = {
+    "sex": "여성",
+    "length": "롱",
+    "style": "레이어드컷",
+    "designer": "디자이너 정재원",
+    "shop_name": "모어온헤어 신논현점",
+    "hashtag1": "C컬펌",
+    "hashtag2": "레이어드펌",
+    "hashtag3": "레이어드컷"
+}
 # save_image_to_db(image_path)
 
 # 예제: 모든 이미지 삭제
@@ -17,7 +28,8 @@ image_path = 'F:/Repository/hAIr_Python/images/Autumn Gaze.jpg'  # 실제 경로
 if __name__ == "__main__":
     # 필요한 함수 호출
     # save_image_to_db(image_path)
-    show_image(15)
+    # show_image(15)
     # list_images()
+    save_image_metadata_to_db(image_path, metadata)
     # delete_all_images()
     # delete_images_by_id_range(1, 10)
--- PostgreSQL/images_utils.py
+++ PostgreSQL/images_utils.py
@@ -1,12 +1,54 @@
 # image_utils.py
 import cv2
 import numpy as np
-from database import get_session, Image
+from database import get_session, Image, ImageMetadata
 
 session = get_session()
 
+# 이미지와 메타데이터를 같이 저장하기
+def save_image_metadata_to_db(file_path, metadata):
+    # 세션 가져오기 각 함수마다 세션을 생성해야지 세션 관리가 쉬워집니다.
+    session = get_session()
+    try:
+        with open(file_path, 'rb') as file:
+            # file_path에 있는 파일을 읽어와 binary_data로 변환
+            binary_data = file.read()
+            # binary_data를 Image 클래스에 넣기
+            new_image = Image(data=binary_data)
+            # new_image를 세션에 넣기
+            session.add(new_image)
+            # 세션 작업 완료
+            session.commit()
+
+            # 메타데이터 저장
+            new_meta = ImageMetadata(
+                # new_image의 아이디를 image id로 저장
+                image_id=new_image.id,
+                sex=metadata["sex"],
+                length=metadata["length"],
+                style=metadata["style"],
+                designer=metadata["designer"],
+                shop_name=metadata["shop_name"],
+                hashtag1=metadata["hashtag1"],
+                hashtag2=metadata["hashtag2"],
+                hashtag3=metadata["hashtag3"]
+            )
+            session.add(new_meta)
+            session.commit()
+
+            print(f"Image and metadata saved with Image ID: {new_image.id}")
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+    except Exception as e:
+        session.rollback()
+        print(f"Error saving image to DB: {e}")
+    finally:
+        session.close()
+
 # 파일 경로를 매개변수로 받아 이미지 파일 저장하기
 def save_image_to_db(file_path):
+    # 세션 가져오기 각 함수마다 세션을 생성해야지 세션 관리가 쉬워집니다.
+    session = get_session()
     try:
         with open(file_path, 'rb') as file:
             binary_data = file.read()
@@ -23,6 +65,8 @@ def save_image_to_db(file_path):
 
 # 모든 이미지 삭제 
 def delete_all_images():
+    # 세션 가져오기 각 함수마다 세션을 생성해야지 세션 관리가 쉬워집니다.
+    session = get_session()
     try:
         images = session.query(Image).all()
         if images:
@@ -40,6 +84,8 @@ def delete_all_images():
 
 # id의 범위 값으로 이미지 지우기
 def delete_images_by_id_range(start_id, end_id):
+    # 세션 가져오기 각 함수마다 세션을 생성해야지 세션 관리가 쉬워집니다.
+    session = get_session()
     try:
         images = session.query(Image).filter(Image.id >= start_id, Image.id <= end_id).all()
         if images:
@@ -57,6 +103,8 @@ def delete_images_by_id_range(start_id, end_id):
 
 # id 값을 기능으로 이미지 가져오기
 def show_image(image_id):
+    # 세션 가져오기 각 함수마다 세션을 생성해야지 세션 관리가 쉬워집니다.
+    session = get_session()
     try:
         # 이미지 데이터베이스에서 읽기
         image = session.query(Image).filter(Image.id == image_id).first()
@@ -76,6 +124,8 @@ def show_image(image_id):
 
 # 데이터베이스에 저장된 이미지 확인
 def list_images():
+    # 세션 가져오기 각 함수마다 세션을 생성해야지 세션 관리가 쉬워집니다.
+    session = get_session()
     try:
         images = session.query(Image).all()
         for image in images:
